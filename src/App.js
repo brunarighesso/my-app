a4146934e779729f237e085e84bc072bfd2cf6ce
brunarighesso <157367684+brunarighesso@users.noreply.github.com>
2024-06-11 11:55:39 +0200

    added h1 and footer

--- src/App.js
+++ src/App.js
@@ -6,9 +6,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1> Weather App</h1>
         <p>
           <Search />
         </p>
+        <footer>
+          Coded by{" "}
+          <a href="https://www.linkedin.com/in/bruna-righesso-23175622/">
+            Bruna Righesso
+          </a>
+          , Open-sourced on
+          <a href="https://github.com/brunarighesso/my-app">GitHub</a>and hosted
+          on <a href="https://wetter-app-mit-react.netlify.app/">Netlify</a>.
+        </footer>
       </header>
     </div>
   );
