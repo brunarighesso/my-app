a40af5eec5c832c06d88f15f2052b9815a9c6e17
brunarighesso <157367684+brunarighesso@users.noreply.github.com>
2024-06-14 12:16:29 +0200

    added css

--- src/App.js
+++ src/App.js
@@ -10,17 +10,16 @@ function App() {
         <p>
           <Search />
         </p>
-        <footer>
-          Coded by{" "}
-          <a href="https://www.linkedin.com/in/bruna-righesso-23175622/">
-            Bruna Righesso
-          </a>
-          , Open-sourced on
-          <a href="https://github.com/brunarighesso/my-app"> GitHub</a> and
-          hosted on{" "}
-          <a href="https://wetter-app-mit-react.netlify.app/">Netlify</a>.
-        </footer>
       </header>
+      <footer>
+        Coded by{" "}
+        <a href="https://www.linkedin.com/in/bruna-righesso-23175622/">
+          Bruna Righesso
+        </a>
+        , Open-sourced on
+        <a href="https://github.com/brunarighesso/my-app"> GitHub</a> and hosted
+        on <a href="https://wetter-app-mit-react.netlify.app/">Netlify</a>.
+      </footer>
     </div>
   );
 }
--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "./Search.css";
 
 export default function Search() {
   const [city, setCity] = useState("");
   const [loaded, setLoaded] = useState(false);
   const [weather, setWeather] = useState({});
+  const [unit, setUnit] = useState("celsius");
 
   function displayWeather(response) {
     setLoaded(true);
@@ -26,11 +28,15 @@ export default function Search() {
   function updateCity(event) {
     setCity(event.target.value);
   }
+  function convertToFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
 
   let form = (
     <form onSubmit={handleSubmit}>
       <input type="search" placeHolder="Type a city" onChange={updateCity} />
-      <input type="submit" value="Search" />
+      <input className="button" type="submit" value="Search" />
     </form>
   );
   if (loaded) {
@@ -38,7 +44,12 @@ export default function Search() {
       <div>
         {form}
         <ul>
-          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>
+            Temperature: {Math.round(weather.temperature)}°C |{" "}
+            <a href="/" onClick={convertToFahrenheit}>
+              °F
+            </a>
+          </li>
           <li>Humidity: {weather.humidity}%</li>
           <li>Wind: {weather.wind}km/h</li>
           <li>
